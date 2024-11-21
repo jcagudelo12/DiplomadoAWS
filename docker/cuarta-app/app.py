bf912a14cb721f54b10edfeed3bd63189dc15af9
Juan Camilo Agudelo <jcagudelo12@gmail.com>
2024-11-21 01:46:57 +0000

    Correción de nombre endpoint

--- docker/cuarta-app/app.py
+++ docker/cuarta-app/app.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/voive', methods=['POST'])
+
+@app.route('/voice', methods=['POST'])
 def vioce():
   try:
     data = request.json
@@ -18,19 +19,18 @@ def vioce():
 
     polly = boto3.client('polly', region_name='us-east-1')
 
-    response = polly.synthesize_speech(
-      Text=text,
-      OutputFormat=output_format,
-      VoiceId=voice
-    )
+    response = polly.synthesize_speech(Text=text,
+                                       OutputFormat=output_format,
+                                       VoiceId=voice)
 
     audio_file = "output.mp3"
     with open(audio_file, 'wb') as file:
       file.write(response['AudioStream'].read())
-      
+
     return send_file(audio_file, mimetype='audio/mpeg')
   except Exception as e:
     return jsonify({'Error': str(e)}), 500
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000)
