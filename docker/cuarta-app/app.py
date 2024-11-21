b7b20ed7d30d4cc08fb0e3c439066869e28d76ac
Juan Camilo Agudelo <jcagudelo12@gmail.com>
2024-11-21 01:23:25 +0000

    Add run

--- docker/cuarta-app/app.py
+++ docker/cuarta-app/app.py
@@ -31,3 +31,6 @@ def vioce():
     return send_file(audio_file, mimetype='audio/mpeg')
   except Exception as e:
     return jsonify({'Error': str(e)}), 500
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000)
